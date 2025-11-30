5191e787f67fb6ca3f10ed7a5502d32f5d5c4d1e
gabrielreis <gabrielreis1103@hotmail.com>
2025-11-30 10:28:56 -0300

    Revert "Estrutura inicial do projeto"
    This reverts commit 2bcd61d0f9638c989b408b7b111803b1186c53bd.

--- include/cdns.php
+++ include/cdns.php
@@ -1,7 +1,7 @@
 <?php
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
---
+++ include/infos.php
@@ -0,0 +1,153 @@
+<?php
+
+// Array com as informações das especialidades
+$especialidades = [
+    [
+        'icon' => 'hugeicons_man.png',
+        'alt' => 'Urologia masculina',
+        'label' => 'Urologia masculina'
+    ],
+    [
+        'icon' => 'streamline-ultimate_pregnancy-sperm-1.png',
+        'alt' => 'Infertilidade',
+        'label' => 'Infertilidade'
+    ],
+    [
+        'icon' => 'hugeicons_woman.png',
+        'alt' => 'Urologia feminina',
+        'label' => 'Urologia feminina'
+    ],
+    [
+        'icon' => 'streamline-ultimate_factory-industrial-robot-arm-1.png',
+        'alt' => 'Cirurgia robótica',
+        'label' => 'Cirurgia robótica'
+    ],
+    [
+        'icon' => 'ph_drop-thin.png',
+        'alt' => 'Disfunções urinárias',
+        'label' => 'Disfunções urinárias'
+    ],
+    [
+        'icon' => 'healthicons_prostate-cancer-outline.png',
+        'alt' => 'Oncologia urológica',
+        'label' => 'Oncologia urológica'
+    ],
+    [
+        'icon' => 'hugeicons_gem.png',
+        'alt' => 'Litíase',
+        'label' => 'Litíase'
+    ],
+    [
+        'icon' => 'healthicons_baby-0203m-outline.png',
+        'alt' => 'Urologia pediátrica',
+        'label' => 'Urologia pediátrica'
+    ]
+];
+
+
+
+// NOVOS ARRAYS PARA LOCAIS DE ATENDIMENTO
+
+$locais_aracaju = [
+    [
+        'id' => 'uniccat',
+        'img_convenio' => 'aracaju/unicat.jpg', // <-- CAMINHO COMPLETO AQUI
+        'img' => 'unicat.png',
+        'tem_convenio' => true, // <-- ADICIONE AQUI
+        'alt' => 'Clínica Uniccat',
+        'titulo' => 'Uniccat',
+        'endereco' => 'Av. Gonçalo Rolemberg Leite, 1813 - Luzia, Aracaju - SE, 49040-280',
+        'telefone' => '(79) 3211-9007'
+    ],
+    [
+        'id' => 'horizonte',
+        'img_convenio' => '', // <-- CAMINHO COMPLETO AQUI
+        'img' => 'jardins.png',
+        'tem_convenio' => false, // <-- ADICIONE AQUI
+        'alt' => 'Clínica Horizonte Jardins',
+        'titulo' => 'Horizonte Jardins',
+        'endereco' => 'Av. Dr. José Machado de Souza, 49 - Jardins, Aracaju - SE, 49025-740',
+        'telefone' => '(79) 3251-1140'
+    ],
+    [
+        'id' => 'hospim',
+        'img_convenio' => 'aracaju/hosprim.jpg', // <-- CAMINHO COMPLETO AQUI
+        'img' => 'hosprim.png',
+        'tem_convenio' => true, // <-- ADICIONE AQUI
+        'alt' => 'Hospital do Rim',
+        'titulo' => 'Hospital do Rim',
+        'endereco' => 'R. Arauá, 92 - Centro, Aracaju - SE, 49010-330',
+        'telefone' => '(79) 3211-9007'
+    ]
+];
+
+$locais_salvador = [
+    [
+        'id' => 'auros',
+        'img_convenio' => 'salvador/auros.jpeg', // <-- CAMINHO COMPLETO AQUI
+        'img' => 'auros.png',
+        'alt' => 'Clínica Auros',
+        'titulo' => 'Clínica Auros',
+        'endereco' => 'Av. Princesa Isabel, 514 - sala 404 - Barra, Salvador - BA, 40130-155',
+        'telefone' => '(71) 3101-2605'
+    ],
+    [
+        'id' => 'cub',
+        'img_convenio' => 'salvador/cub.jpeg', // <-- CAMINHO COMPLETO AQUI
+        'img' => 'cub.png',
+        'alt' => 'Clínica CUB',
+        'titulo' => 'Clínica CUB',
+        'endereco' => 'Rua - Ladeira do Hospital, 248 - Nazaré, Salvador - BA, 40050-420',
+        'telefone' => '(71) 3243-2053'
+    ],
+    [
+        'id' => 'saudemais',
+        'img_convenio' => 'salvador/saudemais.jpeg', // <-- CAMINHO COMPLETO AQUI
+        'img' => 'saudemais.png',
+        'alt' => 'Clínica Saúde Mais',
+        'titulo' => 'Clínica Saúde Mais',
+        'endereco' => 'R. Prof. Agroriniano de Barros, 228 - Caixa d\'Água, Salvador - BA, 40323-010',
+        'telefone' => '(71) 3241-3241'
+    ],
+    [
+        'id' => 'clinica-da-mulher',
+        'img_convenio' => 'salvador/clinicamulher.jpeg', // <-- CAMINHO COMPLETO AQUI
+        'img' => 'clinica-da-mulher.png',
+        'alt' => 'Clínica da Mulher',
+        'titulo' => 'Clínica da Mulher',
+        'endereco' => 'R. Eduardo José dos Santos, 147 - 1006 - Federação, Salvador - BA, 40210-755',
+        'telefone' => '(71) 3237-2633'
+    ],
+    [
+        'id' => 'hospital-portugues',
+        'img_convenio' => 'salvador/hospitalportugues.jpeg', // <-- CAMINHO COMPLETO AQUI
+        'img' => 'hospital-portugues.png',
+        'alt' => 'Centro Médico Hospital Português ',
+        'titulo' => 'Centro Médico Hospital Português ',
+        'endereco' => 'Avenida Princesa Isabel, 914 - Barra Avenida, Salvador - BA, 40140-901',
+        'telefone' => '(71) 3203-5186'
+    ]
+];
+
+
+// ... (seus arrays $especialidades, $locais_aracaju, $locais_salvador já existem aqui) ...
+
+
+// NOVO ARRAY PARA DEPOIMENTOS
+$depoimentos = [
+    [
+        'nome' => 'C. A.',
+        'idade' => '42 anos',
+        'local' => 'Aracaju - SE',
+        'depoimento' => 'Desde a primeira consulta, me senti acolhido e respeitado. O atendimento foi claro, humano e atencioso, transmitindo confiança em cada detalhe. Hoje sigo o tratamento com tranquilidade e gratidão por ter encontrado um profissional que realmente se importa com a minha saúde.'
+    ],
+    [
+        'nome' => 'M. S.',
+        'idade' => '35 anos',
+        'local' => 'Salvador - BA',
+        'depoimento' => 'Profissional exemplar! A Dra. Emily foi muito atenciosa e paciente para explicar todo o procedimento, o que me deixou muito mais calma. Recomendo de olhos fechados.'
+    ]
+];
+
+
+
